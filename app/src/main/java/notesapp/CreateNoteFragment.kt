4696c6aba316684570165b919fdcd451b1cbbf6f
Albert Tazetdinov <zet565758@gmail.com>
2022-07-08 16:51:27 +0300

    файлы по NotesApp

---
+++ app/src/main/java/notesapp/CreateNoteFragment.kt
@@ -0,0 +1,113 @@
+package com.example.notesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.notesapp.database.NotesDatabase
+import com.example.notesapp.entities.Notes
+import kotlinx.android.synthetic.main.fragment_create_note.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class CreateNoteFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+
+        return inflater.inflate(R.layout.fragment_create_note, container, false)
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val currentDate = sdf.format(Date())
+
+        tvDateTime.text = currentDate
+
+        imgDone.setOnClickListener{
+            //saveNote
+            saveNote()
+        }
+
+        imgBack.setOnClickListener{
+            replaceFragment(HomeFragment.newInstance(), false)
+        }
+    }
+
+    private fun saveNote(){
+
+        if (etNoteTitle.text.isNullOrEmpty()){
+            Toast.makeText(context,"Note Title is Required",Toast.LENGTH_SHORT).show()
+        }
+        else if (etNoteSubTitle.text.isNullOrEmpty()){
+
+            Toast.makeText(context,"Note Sub Title is Required",Toast.LENGTH_SHORT).show()
+        }
+
+        else if (etNoteDesc.text.isNullOrEmpty()){
+
+            Toast.makeText(context,"Note Description is Required",Toast.LENGTH_SHORT).show()
+        }
+
+        else{
+
+            launch {
+
+                var notes = Notes()
+                notes.title = etNoteTitle.text.toString()
+                notes.subTitle = etNoteSubTitle.text.toString()
+                notes.noteText = etNoteDesc.text.toString()
+                context?.let {
+                    NotesDatabase.getDatabase(it).noteDao().insertNotes(notes)
+                    etNoteTitle.setText("")
+                    etNoteSubTitle.setText("")
+                    etNoteDesc.setText("")
+                    layoutImage.visibility = View.GONE
+                    imgNote.visibility = View.GONE
+                    tvWebLink.visibility = View.GONE
+                    requireActivity().supportFragmentManager.popBackStack()
+                }
+            }
+        }
+
+    }
+
+
+
+    fun replaceFragment(fragment:Fragment, istransition:Boolean){
+        val fragmentTransition = activity!!.supportFragmentManager.beginTransaction()
+
+        if (istransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.replace(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+    }
+
+}
