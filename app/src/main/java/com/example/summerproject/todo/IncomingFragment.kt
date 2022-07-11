978a18902ba6f56452a2f3cac470abc393b6452c
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-07-12 00:15:58 +0300

    add planList

--- app/src/main/java/com/example/summerproject/todo/IncomingFragment.kt
+++ app/src/main/java/com/example/summerproject/todo/IncomingFragment.kt
@@ -1,47 +1,7 @@
 package com.example.summerproject.todo
 
-import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.summerproject.R
-import com.example.summerproject.databinding.FragmentToDoIncomingBinding
-import com.example.summerproject.todo.db.MyDataBase
-import com.example.summerproject.ui.MainActivity
 
 class IncomingFragment: Fragment (R.layout.fragment_to_do_incoming) {
-
-    private lateinit var binding: FragmentToDoIncomingBinding
-
-    private var adapter :TasksAdapter?  = null
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
-        binding = FragmentToDoIncomingBinding.bind(view)
-
-        adapter = TasksAdapter(MainActivity.getDataBase().getTasks())
-        {
-          it.complete = !it.complete
-
-        }
-
-        binding.rvIncomingTask.adapter = adapter
-
-
-        with(binding){
-            floatingActionButtonAddTask.setOnClickListener(){
-                findNavController().navigate(
-                    R.id.action_incomingFragment_to_addTaskFragment,
-
-                    AddTaskFragment.createBundle(it.id))
-
-
-            }
-        }
-
-    }
-
 }
---
+++ app/src/main/java/com/example/summerproject/todo/plans/list/Plan.kt
@@ -0,0 +1,7 @@
+package com.example.summerproject.todo.plans.list
+
+data class Plan(
+    val id: Int,
+    val task: String,
+    val task_at: String,
+)
